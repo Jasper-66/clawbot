688ef657c326ee792fb19108eb6b92e80a2da1ab
lichanglin <641270002@qq.com>
2026-07-15 14:13:28 +0800

    git 代码测试

--- src/main/java/com/example/mission/task.java
+++ src/main/java/com/example/mission/task.java
@@ -3,5 +3,6 @@ package com.example.mission;
 public class task {
     public static void main(String[] args) {
         System.out.println("Hello world");
+        System.out.println("测试git");
     }
 }
